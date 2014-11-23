3196a8a7458880afde984c33127480c58b5a4a18
Soren Z.Vand <sunrise.freedom@gmail.com>
2014-11-23 15:47:58 +0330

    init Test project for Entity Framwork.

---
+++ test/DF.EntityFramework.Test/DataModels/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using DF.Core.Models;
+
+namespace DF.EntityFramework.Test.DataModels
+{
+    public class Blog : GuidAggregate
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        
+        public virtual List<Post> Posts { get; set; } 
+    }
+}
