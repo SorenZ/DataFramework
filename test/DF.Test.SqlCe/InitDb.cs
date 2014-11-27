3fb4889e39af81999003ed0ceaaaf0d07d8fac29
Soren Z.Vand <sunrise.freedom@gmail.com>
2014-11-27 13:57:20 +0330

    Implemetn RepositoryQueryTests. - Adds some query test.

--- test/DF.Test.SqlCe/InitDb.cs
+++ test/DF.Test.SqlCe/InitDb.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
-
+using System.Collections.Generic;
+using System.Data.Entity;
 using DF.Test.SqlCe.DataModels;
 
 namespace DF.Test.SqlCe
@@ -8,23 +8,60 @@ namespace DF.Test.SqlCe
     public class InitDb
     {
 
+        public static string ConnectionString
+        {
+            get { return string.Format("Datasource = {0}{1}", Environment.CurrentDirectory, @"\test.sdf"); }
+        }
+
+        public static DbContext Init()
+        {
+            SetupDatabase();
+            InsertFakeData();
+
+            return new BloggingContext(ConnectionString);
+        }
+
         public static void SetupDatabase()
         {
-            // file path of the database to create
-            var filePath = Environment.CurrentDirectory + @"\test.sdf";
-
-            // delete it if it already exists
-            if (File.Exists(filePath))
-                File.Delete(filePath);
-
-            // create the SQL CE connection string - this just points to the file path
-            var connectionString = "Datasource = " + filePath;
-
-            using (var context = new BloggingContext(connectionString))
+            
+            using (var context = new BloggingContext(ConnectionString))
             {
-                // this will create the database with the schema from the Entity Model
+                context.Database.Delete();
                 context.Database.Create();
             }
         }
+
+        public static void InsertFakeData()
+        {
+            var context = new BloggingContext(ConnectionString);
+
+            context.Blogs.Add(new Blog
+            {
+                Name = "HANSELMAN",
+                Url = "http://www.hanselman.com/",
+                Posts = new List<Post>
+                {
+                    new Post
+                    {
+                        Title = "ASP.NET 5 (vNext) Work in Progress",
+                        Content = "TagHelpers are a new feature of ASP.NET 5 (formerly and colloquially ASP.NET vNext) but it's taken me (and others) some time to fully digest them and what they mean."
+                    },
+                    new Post
+                    {
+                        Title = "Announcing .NET 2015 - .NET as Open Source",
+                        Content = "It's happening. It's the reason that a lot of us came to work for Microsoft, and I think it's both the end of an era but also the beginning of amazing things to come."
+                    },
+                    new Post
+                    {
+                        Title = "NuGet Package of the Week",
+                        Content = "Yes, really. It's got to be the best name for an open source library out there. It's a great double entendre and a great name for this useful little library. Perhaps English isn't your first language, so I'll just say that a courtesy flush gives the next person a fresh bowl. ;)"
+                    },
+                }
+            });
+
+            context.SaveChanges();
+
+        }
+
     }
 }
---
+++ test/DF.Test.SqlCe/RepositoryQueryTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DF.Contracts;
+using DF.EntityFramework;
+using DF.Test.SqlCe.DataModels;
+using Xunit;
+
+namespace DF.Test.SqlCe
+{
+    public class RepositoryQueryTests
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Post, Guid> _repository;
+
+        public RepositoryQueryTests()
+        {
+            var context = InitDb.Init();
+
+            this._unitOfWork = new UnitOfWork(context);
+            this._repository = _unitOfWork.Repository<Post, Guid>();
+
+        }
+
+        [Fact]
+        public void QueryTest()
+        {
+            var items = this._repository.Query
+                .Where(q => q.Title.Contains(".NET"))
+                .ToList();
+
+            Assert.NotEmpty(items);
+        }
+
+        [Fact]
+        public void GetAllItems()
+        {
+            var items = this._repository.GetAllItems();
+
+            Assert.Equal(3, items.Count());
+        }
+
+        [Fact]
+        public void GetItemByKey()
+        {
+            var key = this._repository.Query
+                .First()
+                .BlogId;
+
+            var item = this._repository.GetItemByKey(key);
+
+            Assert.NotNull(item);
+        }
+
+        [Fact]
+        public void GetItemCountTest()
+        {
+            Assert.Equal(3, this._repository.GetItemCount());
+        }
+
+    }
+}
